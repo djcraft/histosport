0a04c86129ee02637985c42354a1bc2f1aeb8497
djcraft <garzodu47@gmail.com>
2026-03-18 11:44:30 +0100

    Corrige la config Livewire après la mise à jour vers Laravel 12

---
+++ config/livewire.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'component_namespaces' => [
+        'layouts' => resource_path('views/components/layouts'),
+    ],
+    'layout' => 'components.layouts.app',
+    'component_layout' => 'components.layouts.app',
+];
